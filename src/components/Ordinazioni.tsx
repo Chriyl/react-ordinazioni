a679d672d7bce05ab2c305467e58ab2c570c35f3
christian <christianmontanaro390@gmail.com>
2023-08-17 05:10:15 +0400

    add local storage

--- src/components/Ordinazioni.tsx
+++ src/components/Ordinazioni.tsx
@@ -1,88 +1,94 @@
-import React, {FormEvent, useState} from 'react'
-import { FaBitbucket } from "react-icons/fa6" 
-import "./style.css"
+import React, { FormEvent, useState, useEffect } from 'react';
+import { FaBitbucket } from 'react-icons/fa6';
+import './style.css';
 
 interface Order {
-    nome: string;
-    content: string;
-    quantity: number;
+  nome: string;
+  content: string;
+  quantity: number;
 }
 
 const Ordinazioni = () => {
-   const [formm, setFormm] = useState<Order>({
+  const [formm, setFormm] = useState<Order>({
     nome: '',
     content: '',
-    quantity: 0
-   })
+    quantity: 0,
+  });
 
-   const [orders, setOrders] = useState<Order[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
 
-   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setOrders(prev => ([...prev, formm]));
-   }
+  useEffect(() => {
+    const storedOrders = localStorage.getItem('orders');
+    if (storedOrders) {
+      setOrders(JSON.parse(storedOrders));
+    }
+  }, []);
 
-        const handleDelete = (indexdel: number) => {
-            setOrders((orders) => 
-            orders.filter((_, index)=> index !== indexdel)
-            )    
-            
-        }
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newOrders = [...orders, formm];
+    setOrders(newOrders);
+    localStorage.setItem('orders', JSON.stringify(newOrders));
+    setFormm({
+      nome: '',
+      content: '',
+      quantity: 0,
+    });
+  };
+
+  const handleDelete = (indexdel: number) => {
+    const newOrders = orders.filter((_, index) => index !== indexdel);
+    setOrders(newOrders);
+    localStorage.setItem('orders', JSON.stringify(newOrders));
+  };
 
   return (
     <div>
-       <form onSubmit={handleSubmit} className='form'>
-            <input type="text" placeholder='inserisci nome' value={formm.nome} onChange={e => setFormm(prev => ({
-                ...prev,
-                nome: e.target.value
-            }))} />
-            <input type="text" placeholder='inserisci il contenuto' value={formm.content}  onChange={e => setFormm(prev => ({
-                ...prev,
-                content: e.target.value
-            }))}/>
-            <input type="text" placeholder='inserisci il numero' value={formm.quantity}  onChange={e => setFormm(prev => ({
-                ...prev,
-                quantity: +e.target.value
-            }))} />
-            <button type='submit'> vai </button>
-       </form>
+      <form onSubmit={handleSubmit} className='form'>
+        {/* ... your input fields */}
+        <input
+          type="text"
+          placeholder='inserisci nome'
+          value={formm.nome}
+          onChange={(e) => setFormm((prev) => ({ ...prev, nome: e.target.value }))}
+        />
+        <input
+          type="text"
+          placeholder='inserisci il contenuto'
+          value={formm.content}
+          onChange={(e) => setFormm((prev) => ({ ...prev, content: e.target.value }))}
+        />
+        <input
+          type="number"
+          placeholder='inserisci il numero'
+          value={formm.quantity}
+          onChange={(e) =>
+            setFormm((prev) => ({ ...prev, quantity: +e.target.value }))
+          }
+        />
 
-       <table className='tabella'>
-        <tr>
-            <th>
-                nome
-            </th>
-            <th>
-                contenuto
-            </th>
-            <th>
-                quantita
-            </th>
-            <th>
-                gesture
-            </th>
-            
-        </tr>
-        {orders.map((order, index) => (
-            <tr>
-                <th>
-                    {order.nome}
-                </th>
-                <th>
-                    {order.content}
-                </th>
-                <th>
-                    {order.quantity}
-                </th>
-                <th>
-                <button onClick={() => handleDelete(index)}><FaBitbucket/></button>
-                </th>
-                
+        <button type='submit'>vai</button>
+      </form>
+
+      <table className='tabella'>
+      
+        <tbody>
+          {orders.map((order, index) => (
+            <tr key={index}>
+              <td>{order.nome}</td>
+              <td>{order.content}</td>
+              <td>{order.quantity}</td>
+              <td>
+                <button onClick={() => handleDelete(index)}>
+                  <FaBitbucket />
+                </button>
+              </td>
             </tr>
-        ))}
-       </table>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Ordinazioni
+export default Ordinazioni;
